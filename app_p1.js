5479488f63bf413d80c7d86b3d2200a5054fdb71
madhat5 <davidmalary@gmail.com>
2015-10-15 10:53:51 -0400

    pseudo done, layout scheme updated, taking a dive into script syntax

--- app_p1.js
+++ app_p1.js
@@ -1,9 +1,10 @@
 console.log('sim sim salabim');
 
-// BlackJack vs1.
+// BlackJack vs1. //
 
 //////////////////////////////////////////
-//	Variables
+//////////////////////////////////////////
+// Variables //
 
 // deck vars
 // object gameDeck{} with key deck52(calls deckOfCards), shoe, and funtions() 
@@ -14,56 +15,65 @@ console.log('sim sim salabim');
 
 // player vars 
 // array playersAtTable, holds 4max player(call dealer then b2 = aiHand)
-// object player{} with key bankRoll, playerBet, playerHand
-// array playerHand
+// object player{} with key bankRoll (set arbitrarily), playerBet, playerHand
+// array playerHand holds player cards from dealCards
 
 // dealer vars
-// array dealerHand
+// array dealerHand holds dealer cards from dealCards
 // >>>>b2>> aiHand for user option play with ai/players vs dealer mix 
 
 // // gameDeck holds/handles playable deck 
-// var deckCount = 1;
-// var gameShoe = [];
+var deckCount = 1;
+var gameShoe = [];
 
-// var gameDeck = {
-// 	deck52: [],
-// 	deckShuffled: [],
-// 	shuffle: function(deck52) {
-// 		for (var j, x, i = deck52.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-// 			deckShuffled[x]
-// 		return deckShuffled;
-// 	};
-// 	shoe: function() {
-// 		for (var x = 0; x <= deckCount; x++) {
-// 			gameShoe[x] = deckShuffled.splice(x)
-// 		}
-// 		return gameShoe;
-// 	},
-// };
+var gameDeck = {
+	deck52: [],
+	deckShuffled: [],
+	shuffle: function(deck52) {
+		for (var j, x, i = deck52.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+			deckShuffled[x]
+		return deckShuffled;
+	};
+	shoe: function() {
+		for (var x = 0; x <= deckCount; x++) {
+			gameShoe[x] = deckShuffled.pop()
+		}
+		return gameShoe;
+	},
+};
 
 //////////////////////////////////////////
 //////////////////////////////////////////
-// Functions
+// Functions //
 
 // card functions
-// shuffle/randomize (included in gameDeck{} object)
-// create deck function, gives deck52 an array
+// --x--shuffle/randomize (already included in gameDeck{} object)
+// createDeck function, gives deck52 an array
 // softAce function controls ace value (ace = 11; if hand + ace > 21, ace = 1)
 // dealCards takes 2 cards from shoe, gives them to playerHand (splice()) and dealerHand
-// 
 
 // betting functions
-// placeBet takes var userInput bet, subtracts userInput from bankRoll, adds userInput to playerBet
+// placeBet takes var userInput bet, subtracts userInput from bankRoll, adds userInput to playerBet, displays in bet input, calls 
 // calcHand sums current playerHand[x], calculates after 2 cards dealt, and hit
-// hitOrStand asks user after 2 card dealt if want to hit (+1 card) or stay (break)  
-// >>b1>> split function if 2 initial cards === value > splits hand to var splitHand, runs hitOrStand for each card in splitHand
-// compareHand compares current player hand to dealer:
 	// if (initial) playerHand = 21, player wins (1.5x bet) 
-	// if playerHand < 21 && > dealerHand player wins
-	// if  
+	// if playerHand > 21, Bust!
+// hitOrStand asks user after 2 card dealt if want to hit (+1 card) or stay (break) 
+	// >>b1>> after first 2 cards dealt, user has double down option (can also double down on split)
+	// >>b1>> split function if 2 initial cards === value > splits hand to var splitHand, runs hitOrStand for each card in splitHand
+// compareHand compares current player hand to dealer:
+	// if playerHand < 21 && > dealerHand, player wins, add bet x2 to bankRoll 
+	// if playerHand < 21 && < dealerHand, dealer wins
+
+// game functions
+// startGame.click, takes user inputs (human players, ai) and waits for placeBet.click 
+// resetGame.click, sets all game-inputs and bets to null, resets funds, goes to pre-startGame screen
 
 
 
+
+
+//////////////////////////////////////////
+//////////////////////////////////////////
 // MISC NOTES //
 // fund/bet changes as diff player takes turn
 // do i need an empty array for house$ (user money lost)?
